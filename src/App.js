62f70a472c638812fdebba16516bccba09cde775
Andrew Salcedo <asalcedo07@gmail.com>
2017-07-06 16:04:14 -0400

    initial commit: add example theme component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import Theme from './components/Theme'
+
+class App extends Component {
+  render() {
+    return (
+        <Theme />
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Theme.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Theme = () => (
+    <div>
+        <h1>Theme component</h1>
+    </div>
+)
+
+export default Theme 
