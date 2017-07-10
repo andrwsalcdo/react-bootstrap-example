2d49533e94dd520baf8feef5db0cff5cb4f95acf
Andrew Salcedo <asalcedo07@gmail.com>
2017-07-09 22:03:39 -0400

    add navbar comp markup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const Navbar = () => (
-    <div>
-        <h1>Navbar component</h1>
-    </div>
+     <nav className="navbar navbar-default">
+         <div className="container">
+            <div className="navbar-header">
+                <a className="navbar-brand" href="#">Brand</a>
+            </div>
+            <div id="navbar">
+                <ul className="nav navbar-nav">
+                    <li><a className="active"href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>                                        
+                </ul>
+            </div>
+         </div>
+     </nav>
 )
 
 export default Navbar 
