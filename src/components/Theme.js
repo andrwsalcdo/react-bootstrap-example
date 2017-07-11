8bc66ee46d9c6f50aa4fae956c8461371562de1c
Andrew Salcedo <asalcedo07@gmail.com>
2017-07-10 21:50:37 -0400

    change default page, update README

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -9,7 +9,7 @@ class Theme extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            currentPage: 'contact'
+            currentPage: 'home'
         }
     }
     handleChange = (page) => {
